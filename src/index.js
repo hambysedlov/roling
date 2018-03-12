541559e25e76c49d6a20388294137d1525418f30
hambysedlov <hambysedlov@gmail.com>
2018-03-13 01:58:57 +0300

    task 5

---
+++ src/index.js
@@ -0,0 +1,30 @@
+module.exports = function check(string, bracketsConfig) {
+
+        var parentheses = "[]{}()||",
+            stack = [],
+            i, character, bracePosition;
+
+        for(i = 0; character = string[i]; i++) {
+            bracePosition = parentheses.indexOf(character);
+
+            if(bracePosition === -1) {
+                continue;
+            }
+
+            if(bracePosition % 2 === 0) {
+                stack.push(bracePosition + 1);
+            } else {
+                if(stack.length === 0 || stack.pop() !== bracePosition) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.length === 0;
+    if(stack.pop() !== bracePosition) {
+        return false;
+    }else{
+      return true;
+    }
+
+}
